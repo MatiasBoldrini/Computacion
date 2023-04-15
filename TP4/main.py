cec81311ca77053c115adfeda68e02d2e9859e14
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-04-14 23:17:33 -0300

    finished

--- TP4/main.py
+++ TP4/main.py
@@ -1,29 +1,38 @@
 # Definir dos matrices A y B de 2x2
 import os, sys, time
-A = [[1, 2], [3, 4]]
-B = [[5, 6], [7, 8]]
 
-pipe_name = 'operaciones_fifo'
-try:
-    os.mkfifo(pipe_name)
-except FileExistsError:
-    pass
-# Comprobar que las matrices se pueden multiplicar
-resultado = [[0, 0], [0, 0]]
 
-for i in range(2):
-    for j in range(2):
-        pid = os.fork()
-        if pid == 0:
-            for k in range(2):
-                resultado[i][j] += A[i][k] * B[k][j]
-            with open(pipe_name, 'w') as pipe:
-                pipe.write(f'{i}{j}={resultado[i][j]}\n')
-            sys.exit()
+def multiplicar_matrices(matriz_A, matriz_B, pipe_name):
+    try:
+        os.mkfifo(pipe_name)
+    except FileExistsError:
+        pass
+    resultado = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            pid = os.fork()
+            if pid == 0:
+                for k in range(2):
+                    resultado[i][j] += matriz_A[i][k] * matriz_B[k][j]
+                    while True:
+                        try:
+                            with open(pipe_name, 'w') as pipe:
+                                pipe.write(f'{i}{j}={resultado[i][j]}\n')
+                            break
+                        except BrokenPipeError:
+                            pass
+                sys.exit()
 
-with open(pipe_name, 'r') as pipe:
-    linea = ''
-    resultado = {'00': 0, '01': 0, '10': 0, '11': 0}
-    for _ in range(4):
-        linea += pipe.readline()
-        linea = linea.replace('=', ' ').replace('\n', ' ')  
+
+if __name__ == '__main__':
+    A = [[1, 2], [3, 4]]
+    B = [[5, 6], [7, 8]]
+    pipe_name = 'matrices_fifo'
+    multiplicar_matrices(A, B, pipe_name)
+    time.sleep(0.01)
+    with open(pipe_name, 'r') as pipe:
+        linea = ''
+        for _ in range(4):
+            linea += pipe.readline().replace('\n', ' ') 
+        for i in linea.split():
+            print(i) 
