86cc99e84e4f710188c94616e8d713c19caf2310
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-04-15 13:08:21 -0300

    not w

--- TP4/main.py
+++ TP4/main.py
@@ -20,19 +20,25 @@ def multiplicar_matrices(matriz_A, matriz_B, pipe_name):
                                 pipe.write(f'{i}{j}={resultado[i][j]}\n')
                             break
                         except BrokenPipeError:
-                            pass
+                            print('ERROR')
                 sys.exit()
 
 
 if __name__ == '__main__':
-    A = [[1, 2], [3, 4]]
-    B = [[5, 6], [7, 8]]
-    pipe_name = 'matrices_fifo'
-    multiplicar_matrices(A, B, pipe_name)
-    time.sleep(0.01)
-    with open(pipe_name, 'r') as pipe:
-        linea = ''
-        for _ in range(4):
-            linea += pipe.readline().replace('\n', ' ') 
-        for i in linea.split():
-            print(i) 
+    g= 0
+    while(True):
+        A = [[1, 2], [3, 4]]
+        B = [[5, 6], [7, 8]]
+        pipe_name = 'matrices_fifo'
+        multiplicar_matrices(A, B, pipe_name)
+        time.sleep(0.8)
+        with open(pipe_name, 'r') as pipe:
+            linea = ''
+            for _ in range(4):
+                linea += pipe.readline().replace('\n', ' ') 
+            for i in linea.split():
+                print(i) 
+            print(g,'-'*50)
+            if not all(x in linea for x in ["00", "01", "10", "11"]):
+                break
+        g+=1
