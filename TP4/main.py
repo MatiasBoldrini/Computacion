bde8a068f2c59f09584e834f6ea3b6ec75d436ff
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-04-16 11:40:30 -0300

    working.

--- TP4/main.py
+++ TP4/main.py
@@ -1,4 +1,3 @@
-# Definir dos matrices A y B de 2x2
 import os, sys, time
 
 
@@ -14,31 +13,25 @@ def multiplicar_matrices(matriz_A, matriz_B, pipe_name):
             if pid == 0:
                 for k in range(2):
                     resultado[i][j] += matriz_A[i][k] * matriz_B[k][j]
-                    while True:
-                        try:
-                            with open(pipe_name, 'w') as pipe:
-                                pipe.write(f'{i}{j}={resultado[i][j]}\n')
-                            break
-                        except BrokenPipeError:
-                            print('ERROR')
+                with open(pipe_name, 'w') as pipe:
+                    pipe.write(f'{i}{j}={resultado[i][j]}\n')
+
                 sys.exit()
+    sys.exit()
 
 
 if __name__ == '__main__':
-    g= 0
-    while(True):
-        A = [[1, 2], [3, 4]]
-        B = [[5, 6], [7, 8]]
-        pipe_name = 'matrices_fifo'
+    A = [[1, 2], [3, 4]]
+    B = [[5, 6], [7, 8]]
+    pipe_name = '/Users/matiasboldrini/Facu/Computacion/fifo_matrices'
+    pid = os.fork()
+    if pid == 0:
         multiplicar_matrices(A, B, pipe_name)
-        time.sleep(0.8)
-        with open(pipe_name, 'r') as pipe:
-            linea = ''
-            for _ in range(4):
-                linea += pipe.readline().replace('\n', ' ') 
-            for i in linea.split():
-                print(i) 
-            print(g,'-'*50)
-            if not all(x in linea for x in ["00", "01", "10", "11"]):
-                break
-        g+=1
+    time.sleep(0.5)
+    with open(pipe_name, 'r') as pipe:
+        linea = ''
+        for _ in range(4):
+            linea += pipe.readline().replace('\n', ' ') 
+        for i in linea.split():
+            print(i) 
+    os.unlink(pipe_name)
