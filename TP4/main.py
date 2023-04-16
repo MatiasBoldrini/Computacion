aff98bd88b66f511383cb1926e31db1545420056
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-04-16 13:43:09 -0300

    working

--- TP4/main.py
+++ TP4/main.py
@@ -1,6 +1,9 @@
 import os, sys, time
 
-
+"""
+1- Escribir un programa que realice la multiplicación de dos matrices de 2x2. 
+Cada elemento deberá calcularse en un proceso distinto devolviendo el resultado en una fifo indicando el indice del elemento.
+El padre deberá leer en la fifo y mostrar el resultado final."""
 def multiplicar_matrices(matriz_A, matriz_B, pipe_name):
     try:
         os.mkfifo(pipe_name)
@@ -15,23 +18,23 @@ def multiplicar_matrices(matriz_A, matriz_B, pipe_name):
                     resultado[i][j] += matriz_A[i][k] * matriz_B[k][j]
                 with open(pipe_name, 'w') as pipe:
                     pipe.write(f'{i}{j}={resultado[i][j]}\n')
-
                 sys.exit()
-    sys.exit()
 
 
 if __name__ == '__main__':
     A = [[1, 2], [3, 4]]
     B = [[5, 6], [7, 8]]
-    pipe_name = '/Users/matiasboldrini/Facu/Computacion/fifo_matrices'
+    pipe_name = os.getcwd() + 'fifo_matrices'
     pid = os.fork()
     if pid == 0:
         multiplicar_matrices(A, B, pipe_name)
-    time.sleep(0.5)
-    with open(pipe_name, 'r') as pipe:
-        linea = ''
-        for _ in range(4):
-            linea += pipe.readline().replace('\n', ' ') 
-        for i in linea.split():
-            print(i) 
-    os.unlink(pipe_name)
+    else:
+        print(f'Parent: {pid}')
+        os.wait()
+        with open(pipe_name, 'r') as pipe:
+            linea = ''
+            for _ in range(4):
+                linea += pipe.readline().replace('\n', ' ') 
+            for i in linea.split():
+                print(i) 
+        os.unlink(pipe_name)
